abd4f5e8d109d5d01d4bdeca199811f9d13b0d72
cl4r1ty <paul@srcclr.com>
2016-06-29 16:59:20 -0700

    Add requests and django vuln methods

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from __future__ import print_function
 
+import requests
+from django.utils import html
+
 if __name__ == '__main__':
   print('Hello world')
+  r = requests.get('http://www.srcclr.com', allow_redirects=False)
+  print(r.url)
+  print(r.headers)
+  print(r.text)
+  requests.sessions.SessionRedirectMixin.resolve_redirects
+  x = "<html><body>HELLO HTML WORLD</body></html>"
+  print(x)
+  y = html.strip_tags(x)
+  print(y)
