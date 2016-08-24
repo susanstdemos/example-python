6c69196a8b1996caac5ea0326dc9496511d14a1e
Jonathan Tan Chiu Joo <jonathan@srcclr.com>
2016-08-24 16:51:19 +0800

    Error Branch
    Edited code to try to replicate null error status.

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ from rsa import cli
 from feedparser import feedparser
 from requests import sessions
 from lib.Crypto.PublicKey import ElGamal
+from tlslite.tlsrecordlayer import TLSRecordLayer
+
 if __name__ == '__main__':
     formats.get_format()
     algorithms.HMACAlgorithm.prepare_key()
     cli.VerifyOperation.perform_operation()
-    feedparser.parse()
     sessions.SessionRedirectMixin.resolve_redirects()
     ElGamal.generate()
+    feedparser._open_resource()
+    tlsrecordlayer.TLSRecordLayer._decryptRecord()
+    # yo_dawg_i_heard_you_liked_methods()
+
+#
+# def yo_dawg_i_heard_you_liked_methods():
+#     so_we_put_a_method_in_your_method()
+#
+# def so_we_put_a_method_in_your_method():
