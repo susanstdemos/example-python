a3cf49826c98406b6c2129aae1a7feeb1915cde6
Darius Foo <darius.foo.tw@gmail.com>
2016-08-29 17:51:59 +0800

    GPL taint example

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from jwt import algorithms
 from django.utils import formats
 from rsa import cli
 from requests import sessions
+import pyconsts
 
 if __name__ == '__main__':
     formats.get_format()
     algorithms.HMACAlgorithm.prepare_key()
     cli.VerifyOperation.perform_operation()
     sessions.SessionRedirectMixin.resolve_redirects()
+    pyconsts.index()
